8b4a548301d49671453c07d873a795a9f3ed04fe
Ives <ives@ives.me>
2019-02-19 15:41:55 +1100

    Added styles for dragging and hover

--- projects/ngx-mega-simple-drag-drop-list/README.md
+++ projects/ngx-mega-simple-drag-drop-list/README.md
@@ -2,7 +2,7 @@
 
 # NgxMegaSimpleDragDropList
 
-Simple Drag and Drop Reordable List
+Angular 2/3/4/5/6/7 Drag Drop Simple Reordable List Items
 
 ## Screenshot
 
@@ -12,7 +12,7 @@ Simple Drag and Drop Reordable List
 
 ## Demo
 
-https://ivesdotme.github.io/NgxMegaSimpleDragDropListDemoWithNpmPackage/
+https://ives.me/NgxMegaSimpleDragDropListDemoWithNpmPackage/
 
 ## Install
 
@@ -61,8 +61,10 @@ export class AppComponent {
 app.component.html
 
 ```html
-<div class="item" *ngFor="let item of items;let index = index;" megaSimpleDndList [items]="items" [index]="index" (dropped)="onDropped($event)">
-	{{item}}
+<div class="card" *ngFor="let item of items;let index = index;" megaSimpleDndList [items]="items" [index]="index" (dropped)="onDropped($event)">
+	<div class="card-content">
+		{{item}}
+	</div>
 </div>
 ```
 
@@ -70,13 +72,30 @@ app.component.html
 app.component.css
 
 ```css
-.item {
-	width: 200px;
-	border: 1px dashed #808080;
-	margin: 10px;
-	text-align: center;
+.card {
 	cursor: move;
-	font-size: 20px;
-	padding: 20px;
+	float: left;
+	position: relative;
+	margin: .5rem 1rem 1rem 0;
+	background-color: #fff;
+	transition: box-shadow opacity .25s;
+	border-radius: 2px;
+	box-shadow: 0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
+}
+
+.card .card-content{
+	pointer-events: none; /*make sure you add this to all card children. This ensures only the card is dragged*/
+	padding: 40px;
+	border-radius: 0 0 2px 2px;
+	font-size: 30px;	
+	text-align: center;
+}
+
+.hover {
+	box-shadow: 0px 0px 25px 0px rgba(0,0,0,0.75);
+}
+
+.draggin {
+	opacity: 0.4;
 }
 ```
--- projects/ngx-mega-simple-drag-drop-list/src/lib/mega-simple-dnd-list.directive.ts
+++ projects/ngx-mega-simple-drag-drop-list/src/lib/mega-simple-dnd-list.directive.ts
@@ -6,6 +6,8 @@ import { Directive, EventEmitter, HostBinding, HostListener, Input, Output } fro
 export class MegaSimpleDndListDirective {
 	@Input() items: any[];
 	@Input() index: number;
+	@Input() hoverStyle = 'hover';
+	@Input() dragStyle = 'draggin';
 	@Output() dropped = new EventEmitter<any[]>();
 
 	@HostBinding() draggable = true;
@@ -15,21 +17,39 @@ export class MegaSimpleDndListDirective {
 	@HostListener('dragstart', ['$event'])
 	dragstart($event: DragEvent) {
 		$event.dataTransfer.setData('indexSource', this.index.toString());
+		$event.srcElement.classList.add(this.dragStyle);
+	}
+
+	@HostListener('dragend', ['$event'])
+	dragend($event: DragEvent) {
+		$event.srcElement.classList.remove(this.dragStyle);
+	}
+
+	@HostListener('dragenter', ['$event'])
+	dragenter($event) {
+		if (!$event.srcElement.classList.contains(this.dragStyle)) { // Only add the hoverStyle to the other elements
+			$event.srcElement.classList.add(this.hoverStyle);
+		}
+		$event.preventDefault();
+	}
+
+	@HostListener('dragleave', ['$event'])
+	dragleave($event: DragEvent) {
+		$event.srcElement.classList.remove(this.hoverStyle);
 	}
 
 	@HostListener('drop', ['$event'])
 	drop($event: DragEvent) {
+		$event.srcElement.classList.remove(this.hoverStyle);
 		const indexSource = Number($event.dataTransfer.getData('indexSource'));
-
 		[this.items[indexSource], this.items[this.index]] = [this.items[this.index], this.items[indexSource]];
-
 		this.dropped.emit(this.items);
 		$event.preventDefault();
 	}
 
 	@HostListener('dragover', ['$event'])
-	dragover($event: DragEvent) {
-		$event.preventDefault();
+	dragover($event) {
+		$event.preventDefault();  // prevent default to allow drop
 	}
 
 }
